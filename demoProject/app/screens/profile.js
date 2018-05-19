d73717dacdead30c599b99fe8fab8a35a8ea97fd
teun <teunk1@gmail.com>
2018-05-19 18:53:38 +0100

    Added camera API to solution

--- demoProject/app/screens/profile.js
+++ demoProject/app/screens/profile.js
@@ -1,10 +1,18 @@
 /**
  * Created by Teun on 16-5-2018.
  */
-import React from 'react';
-import {Button, Text, View} from "react-native";
+import React, { Component } from 'react';
+import {
+	Button,
+	Text,
+	View,
+	Dimensions,
+	AppRegistry,
+	StyleSheet
+} from "react-native";
+import Camera from 'react-native-camera';
 
-export default class ProfileScreen extends React.Component {
+export default class ProfileScreen extends Component {
 	static navigationOptions = {
 		title: 'Profile',
 		headerStyle: {
@@ -15,11 +23,51 @@ export default class ProfileScreen extends React.Component {
 		}
 	};
 
+	takePicture() {
+		this.camera.capture()
+			.then((data) => console.log(data))
+			.catch(err => console.error(err));
+	}
+
 	render() {
 		return (
-			<View>
-				<Text>profile</Text>
+			<View style={styles.container}>
+				<Camera
+					ref={(cam) => {
+						this.camera = cam;
+					}}
+					style={styles.preview}
+					aspect={Camera.constants.Aspect.fill}>
+					<Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+				</Camera>
 			</View>
 		)
 	}
 }
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	preview: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		height: Dimensions.get('window').height,
+		width: Dimensions.get('window').width
+	},
+	capture: {
+		flex: 0,
+		backgroundColor: '#fff',
+		borderRadius: 5,
+		color: '#000',
+		padding: 10,
+		margin: 40
+	}
+});
+
+// AppRegistry.registerComponent('MyCamera', () => ProfileScreen);
