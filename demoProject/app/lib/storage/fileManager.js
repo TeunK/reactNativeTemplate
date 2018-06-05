12518f26cca4f68cd785d78f820b05d166c461d9
teun <teunk1@gmail.com>
2018-06-05 21:09:22 +0100

    Added function for requesting permissions to write to external storage when required

--- demoProject/app/lib/storage/fileManager.js
+++ demoProject/app/lib/storage/fileManager.js
@@ -5,6 +5,7 @@
 import Realm from 'realm';
 import RNFetchBlob from 'react-native-fetch-blob'
 import Constants from "../../config/constants";
+import { PermissionsAndroid } from 'react-native';
 
 export default fileManager = {
 	writeToMemory: writeToMemory,
@@ -24,25 +25,52 @@ async function loadImage(uri) {
 }
 
 async function writeToMemory(fileData) {
-	let fileName = "";
-	if ('fileName' in fileData){
-		fileName = fileData.fileName;
-	} else if ('uri' in fileData){
-		fileName = getFileNameFromPath(fileData.uri);
-	}
+	const hasWritePermission = await requestWriteExternalDrivePermission();
+	if (hasWritePermission) {
+		let fileName = "";
+		if ('fileName' in fileData){
+			fileName = fileData.fileName;
+		} else if ('uri' in fileData){
+			fileName = getFileNameFromPath(fileData.uri);
+		}
 
-	let imageDataString = "";
-	if ('data' in fileData) {
-		imageDataString = fileData.data;
-	} else if ('base64' in fileData) {
-		imageDataString = fileData.base64;
-	}
+		let imageDataString = "";
+		if ('data' in fileData) {
+			imageDataString = fileData.data;
+		} else if ('base64' in fileData) {
+			imageDataString = fileData.base64;
+		}
 
-	if (fileName !== "" && imageDataString !== "") {
-		let targetImageLocation = Constants.PictureDir + '/' + fileName;
-		await RNFetchBlob.fs.writeFile(targetImageLocation, imageDataString, 'base64');
-		return targetImageLocation;
+		if (fileName !== "" && imageDataString !== "") {
+			let targetImageLocation = Constants.PictureDir + '/' + fileName;
+			await RNFetchBlob.fs.writeFile(targetImageLocation, imageDataString, 'base64');
+			return targetImageLocation;
+		}
+	} else {
+		alert("Error: App does not have permission to write to external storage");
 	}
+}
+
+async function requestWriteExternalDrivePermission() {
+	try {
+		const granted = await PermissionsAndroid.request(
+			PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+			{
+				'title': 'Requesting permission to write to external storage',
+				'message': 'Cannot save file without this permission.'
+			}
+		);
+		if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+			console.log("You can use the camera");
+			return true;
+		} else {
+			console.log("Camera permission denied")
+		}
+	} catch (err) {
+		console.warn(err);
+		return false;
+	}
+	return false;
 }
 
 const getFileNameFromPath = (uri) => {
